2a2f4b08a831890a28547e40514ad6abf8fa4070
Lynxire <ya.staravoitav@gmail.com>
2024-06-22 02:51:32 +0300

    fix exceptions for RestTemplate

--- src/main/java/terabu/shopappcomments/exception/GlobalExceptionHandler.java
+++ src/main/java/terabu/shopappcomments/exception/GlobalExceptionHandler.java
@@ -31,14 +31,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<String> handleResourceException(HttpClientErrorException ex) {
         log.error(ex.getMessage(), ex);
         HttpStatusCode statusCode = ex.getStatusCode();
-        return ResponseEntity.status(statusCode).body(ex.getMessage());
+        return ResponseEntity.status(statusCode).body(ex.getStatusText());
     }
 
     @ExceptionHandler(HttpServerErrorException.class)
     public ResponseEntity<String> handleResourceException(HttpServerErrorException ex) {
         log.error(ex.getMessage(), ex);
         HttpStatusCode statusCode = ex.getStatusCode();
-        return ResponseEntity.body(ex.getMessage());
+        return ResponseEntity.status(statusCode).body(ex.getStatusText());
     }
 
 
